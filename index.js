015aaf9d54693969c6f84a8a8dcd8ba5a9f4df0b
yeongju-chun <yeongju30@naver.com>
2021-02-02 01:02:30 +0900

    비밀번호 암호화(Bcrypt)

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-const express = require('express') //package.json의 의존성에 있는 자료 중, 가져올 자료 선택
-const app = express() //가져온 의존성 실행
+const express = require('express') 
+const app = express() 
 const port = 5000
-const bodyParser = require('body-parser'); //body-parser 가져오기
-const { User } = require('./models/User'); //User 가져오기
+const bodyParser = require('body-parser'); 
+const { User } = require('./models/User'); 
 
 const config = require('./config/key');
 
-//application/x-www/form-urlencoded
-//바디 파싱 설정
 app.use(bodyParser.urlencoded({extended: true})); 
 
-//application/json (응답에서 json을 사용할 수 있도록 설정)
 app.use(bodyParser.json())
 
 
@@ -18,15 +15,11 @@ const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false //에러 방지
 }).then(() => console.log('Mongo DB Connected...'))
- .catch(err => console.log(err)) //확인이 잘 된건지 확인
+ .catch(err => console.log(err)) 
 
 app.get('/', (req, res) => res.send('Hello World!! 안녕하세요!'))
-// 루트 디렉토리에 오면 해당 함수를 실행
-// 클라이언트에 해당 메세지 전달
 
 app.post('/register', (req, res) => {
-    //회원가입할 때 필요한 정보들을  client에서 가져오면
-    //그것들을 데이터 베이스에 넣어준다.
     
     const user = new User(req.body);
 
@@ -34,12 +27,11 @@ app.post('/register', (req, res) => {
         if(err) return res.json({ success : false, err}) // 실패 시
         return res.status(200).json({
             success: true
-        }) // 성공 시 
-    }); // save(콜백함수)는 MongoDB에서 오는 함수
+        })
+    }); 
 
 })
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-// 리스너가 해당 포트에 접근을 인지하면 터미널에 해당 작업을 함
 
--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
     name : {
@@ -30,6 +32,25 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 })
+
+//pre() > MongoDB에서 가져온 함수 특정 함수를 실행하기 이전에 실행되는 함수
+userSchema.pre('save', function(next) {
+    // 비밀번호 암호화
+    var user = this;
+    
+    if(user.isModified('password')) {
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            if(err) return next(err); // 에러 시, stop
+    
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if(err) return next(err); // 에러 시, stop
+                user.password = hash;
+                return next(); // 에러가 나지 않았다면, plain password를 hash로 바꿔줌
+            }); 
+        });
+    }
+
+}) 
 
 const User = mongoose.model('User', userSchema)
 
