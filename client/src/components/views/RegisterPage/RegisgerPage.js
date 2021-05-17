94ab3a699f3590bd27d2590612f7f9bfd60fc9ec
yeongju-chun <yeongju30@naver.com>
2021-05-18 01:06:44 +0900

    리액트 폴더 설정 변경

---
+++ client/src/components/views/RegisterPage/RegisgerPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function RegisgerPage() {
+    return (
+        <div>
+            RegisgerPage
+        </div>
+    )
+}
+
+export default RegisgerPage
--- client/src/index.js
+++ client/src/index.js
@@ -6,10 +6,11 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App /> // import한 App을 태그처럼 이용해서 붙임
   </React.StrictMode>,
   document.getElementById('root')
 );
+// ReactDOM.render(들어갈 내용, index.html의 아이디)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
